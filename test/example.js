f15352506d039496dff0498702535dc50c819c5b
Andrew Branch <andrew@wheream.io>
2023-02-23 19:19:42 -0800

    Fix stuff, add types, add ESM wrapper

--- test/example.js
+++
@@ -1,6 +0,0 @@
-var untar = require('../lib/untar.js')
-var fs = require('fs')
-
-untar.untar(fs.readFileSync(__dirname + '/' + 'test.tar')).forEach(function(file){
-    console.log(file.name)
-})
--- test/pako.js
+++
@@ -1,9 +0,0 @@
-var untar = require('../lib/untar.js')
-var pako = require('pako')
-var fs = require('fs')
-
-var unzipped = pako.inflate(fs.readFileSync(__dirname + '/' + 'with-5.0.0.tgz'));
-
-untar.untar(unzipped).map(function(file){
-	console.log(file.filename)
-})
